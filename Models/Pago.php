50d9e453951c08ebe88479be2e38751c29321d34
Oscar  Camacho <OscarCamacho@Oscars-Mac-Pro.local>
2017-05-09 08:53:41 -0500

    Sprint 3: ++*

---
+++ Models/Pago.php
@@ -0,0 +1,25 @@
+<?php namespace APP\Models;
+
+
+class Punto extends BaseModel
+{
+
+    protected   $_tableName = "Puntos";
+    protected   $idPunto;
+    protected   $fecha;
+    protected   $hora;
+    protected   $estadoDireccion;
+    protected   $delegacionMunicipioDireccion;
+    protected   $codigoPostalDireccion;
+    protected   $calleNumeroDireccion;
+    protected   $descripcionDireccion;
+    protected   $idViaje;
+
+}
+
+/*
+COMENTARIOS GENERALES:
+
+*/
+
+ 
